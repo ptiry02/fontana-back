4bb6ea547eed6c80b052269886bfbbd6ee573316
Paul Tiry <tirypaul@gmail.com>
2022-11-17 17:40:03 +0100

    setup env variables with dotenv

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+import {} from 'dotenv/config'
 import express from 'express'
 import appConfig from './config/index.config.js'
 import connectDB from './database/index.db.js'
+
+// Start enviroment variables config.
+
+console.log('Mongo URI: ', process.env.MONGO_URI)
 
 // Start express framework
 const app = express()
