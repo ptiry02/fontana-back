1ecd076108519f0f381865926918bac739d92840
Paul Tiry <tirypaul@gmail.com>
2022-12-14 18:40:39 +0100

    changed home route response

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,7 @@ import foodsRouter from './food.routes.js'
 const allRoutes = Router()
 
 allRoutes.get('/', (req, res) => {
-  res.json({ endpoints: 'food/es for recipes in Spanish and food/cat for recipes in catalan' })
+  res.json({ endpoints: 'comming soon...' })
 })
 
 allRoutes.use('/food', foodsRouter)
